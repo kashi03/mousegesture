a3e8473a7ae4daaa4de6203640faef3a4f472b9a
kashi <24215309+kashi03@users.noreply.github.com>
2025-06-03 13:47:32 +0900

    ジェスチャーのリセットを行うように修正

--- content.js
+++ content.js
@@ -58,6 +58,7 @@
 
   const gestureEnd = () => {
     ctx.clearRect(0, 0, gestureCanvas.width, gestureCanvas.height)
+    gesturePath = [];
   }
 
   document.addEventListener("contextmenu", (event) => {
